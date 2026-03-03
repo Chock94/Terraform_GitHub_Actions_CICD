2ba4463a88d0f6f50a6079801203b766a1b32cca
Chock94 <teran.hotcackes@gmail.com>
2026-03-03 09:47:57 -0600

    Update DynamoDB table name in providers.tf

--- providers.tf
+++ providers.tf
@@ -8,7 +8,7 @@ terraform {
     bucket         = "carlos-s3bucket-03032026"
     key            = "globalstate/s3/terraform.tfstate"
     region         = "eu-west-1"
-    dynamodb_table = "carlos-03032026-dblocks"
+    dynamodb_table = "carlos-030320261-dblocks"
     encrypt        = true
   }
 }
